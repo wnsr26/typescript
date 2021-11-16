932d95b7007285787a5314266cdabb2c305322ea
wnsr26 <phutter759@gmail.com>
2021-11-16 02:29:11 -0800

    first commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -5,6 +5,46 @@ import { Component } from '@angular/core';
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
+
 export class AppComponent {
-  title = 'NG101';
+  user = { id:1001, name:"Nantawat" ,address:"Thailand"}
+  
+  i:number=0;
+  nameInput:string="";
+
+  sayHi(){
+    return "Hi from " + this.user.name;
+  }
+
+  addCount(){
+    this.i++;
+  }
+
+  delCount(){
+    this.i--;
+  }
+
+  readName(name:string){
+    console.log(name);
+    this.nameInput = name;
+  }
+
+  valid: boolean = 1>2;
+  validAge: boolean=false;
+
+  readAge(age:string){
+    let a = parseInt(age);
+    this.validAge = a>=18 || a<=25;
+ }
+
+ menus = ["Shabu","KFC","MK","BBQ","Salad"];
+
+ courses = [
+   {id:1, title:"TypeScript"},
+   {id:2, title:"Java"},
+   {id:3, title:"PHP"},
+   {id:4, title:"Angular"},
+   {id:5, title:"Flutter"}
+ ];
+
 }
